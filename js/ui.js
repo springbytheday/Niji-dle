a599b6ffbd6d7d23e6079c4e1854a7a3ff9b2988
YiQingK <1218qing@gmail.com>
2026-07-10 23:30:38 +0800

    fun fact link

--- js/ui.js
+++ js/ui.js
@@ -255,6 +255,7 @@ function showEndState() {
         <p class="end-title">It was ${escapeHtml(answerliver.name)}!</p>
         <p class="end-sub">New liver in: <span id="countdown-display" class="countdown-display">00:00:00</span></p>
         <button id="share-button" class="share-button" type="button">Share results</button>
+        <a class="end-link" href="https://forms.gle/pbkSTggPu1ghMwae8">Share some fun facts about your fav livers~</a>
       </div>`;
     banner.classList.add('visible');
  
